ab79ee7f487fb29f24b01faba3966a55e1f6dac7
Ryan Flores <Ryan.carneiro.flores@gmail.com>
2021-04-30 16:43:46 -0300

    inclusão de cadastro de usuário no bd localhost

---
+++ public/dashboard/conectar.php
@@ -0,0 +1,8 @@
+<?php
+/* Dados do Banco de Dados a conectar */
+$Servidor = 'localhost';
+$nomeBanco = 'analitics';
+$Usuario = 'root';
+$Senha = '123';
+$strcon = mysqli_connect($Servidor, $Usuario, $Senha, $nomeBanco); 
+?>      
