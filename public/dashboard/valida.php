4c2ae90c09e9fd0f62cc22d1a2875ec5c095b78d
Ryan Flores <Ryan.carneiro.flores@gmail.com>
2021-05-06 17:34:49 -0300

    inclusão de login, mas tem um erro na variavel query, verificar depois

---
+++ public/dashboard/valida.php
@@ -0,0 +1,18 @@
+<?php
+    include_once("conectar.php");
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+
+  // Validação do usuário/senha digitados
+  $sql = "SELECT 'id', 'nome', 'nivel' FROM 'usuarios' WHERE ('usuario' = '".$usuario ."') AND ('senha' = '". sha1($senha) ."') AND ('ativo' = 1) LIMIT 1";
+  $query = mysqli_query($sql); // essa linha está dando erro, verificar  
+  if (mysqli_num_rows($query) != 1) {
+      // Mensagem de erro quando os dados são inválidos e/ou o usuário não foi encontrado
+      echo "Login inválido!"; exit;
+  } else {
+      // Salva os dados encontados na variável $resultado
+      $resultado = mysqli_fetch_assoc($query);
+  }
+
+  ?>
